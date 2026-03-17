4095e1f642844aaa7af7c06e2159ed036de41e9a
hexen <57230240+LavaWander@users.noreply.github.com>
2026-03-17 16:19:08 +0200

    Added player and movement (running/walking)

---
+++ Game/player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+@export var base_speed := 200
+var speed = base_speed
+
+func _physics_process(_delta):
+	speed = base_speed # resets speed
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var is_running = Input.is_action_pressed("run")
+	
+	if is_running:
+		speed = base_speed * 1.5
+	velocity = direction * speed
+	move_and_slide()
+	
+	# handle animation
+	var sprite = $AnimatedSprite2D
+	if direction != Vector2.ZERO:
+		if is_running:
+			sprite.animation = "run"
+		else:
+			sprite.animation = "walk"
+		
+		sprite.play()
+		
+		# flip horizontally depending on direction.x
+		if direction.x != 0:
+			sprite.flip_h = direction.x < 0
+	else:
+		sprite.animation = "idle"
+		sprite.play()
